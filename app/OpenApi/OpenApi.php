defe80f070b2694f9d9a38e50f27e07f7d1b09af
Gismat Mammadov <gismat.mammadov@gfmoney.com>
2026-06-18 01:56:11 +0400

    Adding Swagger/OpenAPI documentation

---
+++ app/OpenApi/OpenApi.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\OpenApi;
+
+/**
+ * @OA\Info(
+ *     title="Messaging API",
+ *     version="1.0.0",
+ *     description="API documentation for Laravel messaging assessment project"
+ * )
+ *
+ * @OA\Server(
+ *     url="http://localhost:8000",
+ *     description="Local Docker environment"
+ * )
+ */
+class OpenApi
+{
+}
